5f046f9130c6c937f4e278eb1f656c344f22bec3
isabelalucas <isabelalucas@outlook.com>
2022-04-06 16:33:07 -0700

    Fixed feedback on visualize_vars and added tests

--- tests/testthat/test-visualize_vars.R
+++ tests/testthat/test-visualize_vars.R
@@ -1,3 +1,12 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+test_that("Var does not exist", {
+  expect_error(visualize_vars.R(mtcars, dne))
+})
+
+test_that("`visualize_vars` returns a gg", {
+  expect_s3_class(visualize_vars(mtcars, mpg, disp), class = "gg")
+})
+
