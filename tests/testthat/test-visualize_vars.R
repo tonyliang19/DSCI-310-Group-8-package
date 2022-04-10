e418659692eb1779b7f53c8b9a9f2a0e03bd8ded
tonyliang19 <chunqingliang@gmail.com>
2022-04-09 18:51:07 -0700

    added tests for visualize_vars

--- tests/testthat/test-visualize_vars.R
+++ tests/testthat/test-visualize_vars.R
@@ -1,6 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# Test inputs
+wrong_df <- seq(1:10)
+wrong_x <- data.frame(x = "aaa", y = 1)
+wrong_y <- data.frame(x = 1, y = "aaa")
+
+
+test_that("df should be a data frame or data frame extension",{
+  expect_error(visualize_vars(wrong_df, 1,2))  
 })
+
+test_that("x and y should be of type numeric, x is not numeric",{
+  expect_error(visualize_vars(wrong_x, x,y))  
+})
+
+test_that("x and y should be of type numeric, y is not numeric",{
+  expect_error(visualize_vars(wrong_y, x,y))  
+})
+
 
 test_that("Var does not exist", {
   expect_error(visualize_vars.R(mtcars, dne))
